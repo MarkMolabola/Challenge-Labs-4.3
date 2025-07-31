3b9b21de55b951914e850f2fdcbab3c6dff1066e
MarkMolabola <mark.molabola@outlook.com>
2025-07-31 10:06:55 -0700

    update

--- Program.cs
+++ Program.cs
@@ -6,26 +6,23 @@ namespace Challenge_Labs_4._3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a Word: ");
-            string input = Console.ReadLine();
-            ReverseString(input);
-            Console.WriteLine($"Reversed: {ReverseString(input)}");
-
-
-        }
-        static string ReverseString(string input)
-        {
-            char[] word = input.ToCharArray();
-
-            for (int i = 0, j = input.Length - 1; i < j; i++, j--)
+            string word = "Hello World";
+            char[] letters = word.ToCharArray();
+            ReverseString(letters);
+            Console.WriteLine("Reversed Word: ");
+            for (int i = 0; i < letters.Length; i++)
             {
-                
+                Console.Write(letters[i]);
+            }
+        }
+        static void ReverseString(char[] word)
+        {
+            for (int i = 0, j = word.Length - 1; i < j; i++, j--)
+            {
                 char temp = word[i];
                 word[i] = word[j];
                 word[j] = temp;
-
             }
-            return new string (word);
         }
 
 
